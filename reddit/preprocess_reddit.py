2247a810175fb39e1ab0fdfcd174c8e91590ee28
Sean La <seanla1996@gmail.com>
2020-12-13 22:50:51 -0800

    add preprocessing of new reddit data, and update gitignore with ignoring of ds store

--- reddit/preprocess_reddit.py
+++ reddit/preprocess_reddit.py
@@ -6,21 +6,25 @@ from sklearn.model_selection import train_test_split
 random.seed(0)
 np.random.seed(0)
 
-df = pd.read_json("reddit_sentiment_results.json").T
-sentiment = pd.json_normalize(df.sentiment_all)
-for column in sentiment:
-    df.loc[:, column] = sentiment[column].to_numpy()
+for filename in ['reddit', 'COVID19positive', 'childfree', 'okcupid']:
+    path = "{}_sentiment_results.json".format(filename)
 
-df.loc[:, 'sentiment'] = (df['pos'].to_numpy() > df['neg'].to_numpy()) & (df['pos'].to_numpy() > df['neu'].to_numpy())
-df.loc[:, 'sentiment'] = df['sentiment'].astype(int)
-
-df = df[['comment', 'score', 'sentiment']]
-
-train, test = train_test_split(df, train_size=0.8, random_state=0)
-test, dev = train_test_split(test, train_size=0.5, random_state=0)
-
-df.loc[train.index, "split"] = "train"
-df.loc[test.index, "split"] = "test"
-df.loc[dev.index, "split"] = "dev"
-
-df.to_csv('reddit_sentiment_processed.csv')
+    df = pd.read_json(path).T
+    sentiment = pd.json_normalize(df.sentiment_all)
+    for column in sentiment:
+        df.loc[:, column] = sentiment[column].to_numpy()
+    
+    df.loc[:, 'sentiment'] = (df['pos'].to_numpy() > df['neg'].to_numpy()) & (df['pos'].to_numpy() > df['neu'].to_numpy())
+    df.loc[:, 'sentiment'] = df['sentiment'].astype(int)
+    
+    df = df[['comment', 'score', 'sentiment']]
+    
+    train, test = train_test_split(df, train_size=0.8, random_state=0)
+    test, dev = train_test_split(test, train_size=0.5, random_state=0)
+    
+    df.loc[train.index, "split"] = "train"
+    df.loc[test.index, "split"] = "test"
+    df.loc[dev.index, "split"] = "dev"
+    
+    output = "{}_sentiment_processed.csv".format(filename)
+    df.to_csv(output)
